f3022e0f524a345dd26b0e89d348abe2c5fe5648
damn-boi <bohemianjerk@gmail.com>
2023-07-19 15:22:33 +0300

    Corrected init.sql and added info to readme.md

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# data-mart
 # 1T_2.8
-# 1T_2.8
-# 1T_2.8
+
+Rebuilding existing Postrges data-mart with Clickhouse
+
+Accessing data-mart code: [.db/dml/queries.sql](https://github.com/damn-boi/1T_2.8/blob/main/db/dml/queries.sql)
--- db/ddl/init.sql
+++ db/ddl/init.sql
@@ -1,8 +1,56 @@
 CREATE TABLE IF NOT EXISTS products 
 (
-    product_id Int32,
-    product_name String,
-    price Float32
+    product_id Int32 NOT NULL,
+    product_name String NOT NULL,
+    price Float32 NOT NULL
 )
 ENGINE = MergeTree()
 ORDER BY product_id;
+
+CREATE TABLE IF NOT EXISTS shops
+(
+    shop_id Int32 NOT NULL,
+    shop_name String NOT NULL
+)
+ENGINE = MergeTree()
+ORDER BY shop_id;
+
+CREATE TABLE IF NOT EXISTS plan
+(
+    product_id Int32 NOT NULL,
+    shop_id Int32 NOT NULL,
+    plan_cnt Int32 NOT NULL,
+    plan_date Date NOT NULL
+)
+ENGINE = MergeTree()
+ORDER BY (product_id, shop_id);
+
+CREATE TABLE IF NOT EXISTS shop_dns
+(
+    shop_id Int32 NOT NULL,
+    date Date NOT NULL,
+    product_id Int32 NOT NULL,
+    sales_cnt Int32 NOT NULL
+)
+ENGINE = MergeTree()
+ORDER BY (shop_id, product_id);
+
+CREATE TABLE IF NOT EXISTS shop_mvideo
+(
+    shop_id Int32 NOT NULL,
+    date Date NOT NULL,
+    product_id Int32 NOT NULL,
+    sales_cnt Int32 NOT NULL
+)
+ENGINE = MergeTree()
+ORDER BY (shop_id, product_id);
+
+CREATE TABLE IF NOT EXISTS shop_sitilink
+(
+    shop_id Int32 NOT NULL,
+    date Date NOT NULL,
+    product_id Int32 NOT NULL,
+    sales_cnt Int32 NOT NULL
+)
+ENGINE = MergeTree()
+ORDER BY (shop_id, product_id);
