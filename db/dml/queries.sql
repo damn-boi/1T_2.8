9514ecbb90f8787dbe142a7a9cc41e9dd6bd35ae
damn-boi <bohemianjerk@gmail.com>
2023-07-20 13:59:25 +0300

    Added plan-date column to data-mart

--- db/dml/queries.sql
+++ db/dml/queries.sql
@@ -14,10 +14,15 @@ shop_sales AS (
         FROM all_shop_sales ss
         INNER JOIN shops USING (shop_id)
         GROUP BY shop_name, product_id, shop_id, month
-)
+),
+plan_with_accurate_month AS (
+		SELECT product_id, shop_id, plan_cnt, 
+		toMonth(plan_date) as month, plan_date
+		from plan)
 SELECT
     ss.shop_name,
     pr.product_name,
+    pl.plan_date,
 	ss.sales AS sales_fact,
 	pl.plan_cnt AS sales_plan,
 	ss.sales / pl.plan_cnt AS sales_ratio,
@@ -26,5 +31,6 @@ SELECT
     ss.sales * pr.price / (pl.plan_cnt * pr.price) AS income_ratio
 FROM
 	shop_sales AS ss
-	JOIN plan AS pl ON ss.shop_id = pl.shop_id AND ss.product_id = pl.product_id
+	JOIN plan_with_accurate_month AS pl ON ss.shop_id = pl.shop_id AND ss.product_id = pl.product_id
+	AND ss.month = pl.month
 	JOIN products AS pr ON ss.product_id = pr.product_id;
